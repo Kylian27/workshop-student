47bc46cdbb90e08de19f962ca4a5dd755e3d1e15
Janiaut <thomas.janiaut@epitech.eu>
2022-11-23 16:42:36 +0100

    add_findstr_compare-lenght-of-needle-with-strlen-and-j-j-increase-when-carac-found

---
+++ src/string/findstr.c
@@ -0,0 +1,14 @@
+
+#include "strlen.h"
+char *findstr( char *haystack, const char *needle)
+{
+    int j = 0;
+    for (int i = 0; haystack[i] != 0; i++) {
+        if (haystack[i] == needle[j])
+            j++;
+        if (j == strlen(needle)) {
+            return haystack + (i - j);
+        }
+    }
+    return NULL;
+}
---
+++ src/string/findstr.h
@@ -0,0 +1,6 @@
+#ifndef FINDSTR_H
+    #define FINDSTR_H
+
+char *findstr(char *haystack, const char *needle);
+
+#endif //FINDSTR_H
